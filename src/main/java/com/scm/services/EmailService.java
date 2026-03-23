34c2e1aab30edc1926f0a3e3c620a7c3f80f23d6
Chandanatech01 <chandanaaerupula@gmail.com>
2026-03-23 09:05:11 +0530

    Fixed service interfaces

--- src/main/java/com/scm/services/EmailService.java
+++ src/main/java/com/scm/services/EmailService.java
@@ -1,19 +1,7 @@
 package com.scm.services;
 
 public interface EmailService {
-
-    // Send plain text email
     void sendEmail(String to, String subject, String body);
-
-    // Send HTML formatted email
-    void sendEmailWithHtml(String to, String subject, String htmlBody);
-
-    // Send email with attachment
-    void sendEmailWithAttachment(String to, String subject, String body, String attachmentPath);
-
-    // Send email verification link
-    void sendVerificationEmail(String to, String verificationLink);
-
-    // Send welcome email after registration
-    void sendWelcomeEmail(String to, String name);
+    void sendEmailWithHtml();
+    void sendEmailWithAttachment();
 }
--- src/main/java/com/scm/services/ImageService.java
+++ src/main/java/com/scm/services/ImageService.java
@@ -3,13 +3,6 @@ package com.scm.services;
 import org.springframework.web.multipart.MultipartFile;
 
 public interface ImageService {
-
-    // Upload image and return URL
-    String uploadImage(MultipartFile image, String filename);
-
-    // Get image URL from Cloudinary public ID
+    String uploadImage(MultipartFile file, String fileName);
     String getUrlFromPublicId(String publicId);
-
-    // Delete image from Cloudinary by public ID
-    void deleteImage(String publicId);
 }
